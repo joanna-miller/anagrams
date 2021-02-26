7d03722b8adb91cbd372fae33aef20fcf192c66a
Jo <joannadawnmiller@gmail.com>
2021-02-26 12:50:59 -0500

    refactored test to have vowel_check method, pass test to check string for vowels

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,11 +1,19 @@
-class Words
+class Words < String
   def initialize(str1, str2)
     @words = [str1, str2]
   end
 
-  def receiver
+  def words
     @words
   end 
+
+  def self.vowel_check(str)
+    if str.match(/[aeiouy]/)
+      return true
+    else
+      return false
+    end
+  end
 
   def anagram_check()
 
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -2,17 +2,25 @@ require 'rspec'
 require 'anagrams'
 
 describe(Words) do
-  describe('#receiver') do
+  describe('#words') do
     it('reads the value of a new Words instance') do
-      words = Words.new("cat", "dog")
-      expect(words.receiver()).to(eq(["cat", "dog"]))
-    end
-  end
-  describe('#anagram_check') do
-    it('checks that input contains at least one vowel or y') do
-      words = Words.new("jnn", "mllr")
-      expect(words.anagram_check()).to(eq("Please input actual words!"))
+      user_input = Words.new("cat", "dog")
+      expect(user_input.words()).to(eq(["cat", "dog"]))
     end
   end
   
+  describe('#vowel_check') do
+    it('checks that a string contains at least one vowel or y') do
+      expect(Words.vowel_check("jnn")).to(eq(false))
+      expect(Words.vowel_check("cat")).to(eq(true))
+    end
+  end
+
+  describe('#anagram_check') do
+    it('') do
+      user_input = Words.new("", "")
+      expect(user_input.anagram_check()).to(eq("P"))
+    end
+  end
+
 end
