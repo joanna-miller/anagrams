39bf11312b340eaab28febdaab8c320052c7f2c7
Jo <joannadawnmiller@gmail.com>
2021-02-26 11:26:39 -0500

    initialize instance variable and use rspec to confirm that reciever method is linked to spec

---
+++ lib/anagrams.rb
@@ -0,0 +1,9 @@
+class Words
+  def initialize(str1, str2)
+    @words = [str1, str2]
+  end
+
+  def receiver
+
+  end 
+end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,2 +1,11 @@
 require 'rspec'
 require 'anagrams'
+
+describe(Words) do
+  describe("#receiver") do
+    it('reads the value of a new Words instance') do
+      words = Words.new("cat", "dog")
+      expect(words.receiver()).to(eq(["cat", "dog"]))
+    end
+  end
+end
